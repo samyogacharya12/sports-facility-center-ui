20d5a45421e5aa88250e0c117045fdcd537b693e
Samyog Acharya <samyog.acharya@gmail.com>
2025-10-23 05:04:05 +0545

    made images and layout more clear

--- src/app/components/facilities/facilities.component.ts
+++ src/app/components/facilities/facilities.component.ts
@@ -62,6 +62,43 @@ facilities: Facility[] = [];
        end: '13:00'
      },
        ]
+     },
+          {
+       id: 4,
+       facilityName: 'Basketball Court',
+       description: 'Clay court maintained daily for high performance play.',
+       imageUrl: '/home/samyog/samyog/sports-facility-center-ui/src/assets/tennis.jpeg',
+       capacity: 2,
+       openingTime: '08:00 AM',
+       closingTime: '10:00 PM',
+       timeSlots: [{
+       start: '11:00',
+       end: '12:00'
+     },{
+       start: '12:00',
+       end: '13:00'
+     },
+     {
+       start: '13:00',
+       end: '14:00'
+     },
+      {
+       start: '14:00',
+       end: '15:00'
+     },
+      {
+       start: '15:00',
+       end: '16:00'
+     },
+      {
+       start: '16:00',
+       end: '17:00'
+     },
+      {
+       start: '17:00',
+       end: '18:00'
+     }
+       ]
      }
    ];
 
